62b9c4a04ceac20c0eda8593cadf90e86c31ce35
Aman Verma <aman@Amans-MacBook-Air.local>
2023-01-30 14:40:47 +0530

    Add apis to home js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,30 +2,34 @@ import React from 'react'
 
 function Home() {
 
-  const apiKey = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7097e4d7bdmshee9a06b5147d569p18048ejsn1cdc6692bd58',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-  };
-
-const cityName = 'dhar'
-  async function fetchData(city,options){
-    
- try {
-    const res =  await fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,options);
-   const data = await res.json()
-   console.log(data);
- } catch (error) {
-  console.log(error);
-  
- }
+  const apiKey='ad00226a52d14ad0fd5c6959889baa7e'
+  async function fetchGeoCordinate(city){
+      const geoCordinateUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${3}&appid=${apiKey}`
+    try {
+      const res = await fetch(geoCordinateUrl);
+      const data = await res.json()
+      fetchWeatherData(data[0].lat,data[0].lon)     
+    }
+     catch (error) {
+      console.log(error);     
+    }
   }
-  fetchData(cityName,apiKey)
+  async function fetchWeatherData(lat ,lon){
+     const apiUrl =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+     try {
+      const res = await fetch(apiUrl);
+      const data = await res.json()
+      console.log(data);     
+     } catch (error) {
+      console.log(error);     
+     }
+  }
+  fetchGeoCordinate('Dhar')
+
   return (
-    <div>
-Aman
+    <div className='Home'>
+      
+
     </div>
   )
 }
